10955ad6cd72fc60f71f3e071c15ab5d7a019f26
Harsh Vardhan Vashistha <hvvashistha@gmail.com>
2016-12-08 01:35:43 -0500

    Adding some default setting, legends, contributor lists

--- github-commit-summary/scripts/githubAPI.js
+++ github-commit-summary/scripts/githubAPI.js
@@ -42,15 +42,40 @@ function getRepoInfo(repoName) {
 }
 
 function getProcessedData(settings) {
-    numberOfAuthors = settings.maxCollaborators || 8;
-    numberOfWeeks = settings.weeksInSegments || 21;
-
+    maxWeeks = settings.maxNumberOfSegments * settings.weeksInSegments;
+    lastWeek = 0;
+    weeksSize = null;
+    maxCommits = 0;
     return getRepoInfo(settings.repoName).then(function(data){
-        data = data.slice(Math.max(data.length - numberOfAuthors - 1, 0), data.length - 1);
+        data = data.slice(Math.max(data.length - settings.maxCollaborators - 1, 0), data.length - 1);
         data.forEach(function(author){
-            author.weeks = author.weeks.slice(Math.max(author.weeks.length - numberOfWeeks - 1, 0), author.weeks.length - 1);
+            if (weeksSize == null) {
+                weeksSize = author.weeks.length;
+                maxWeeks = Math.min(maxWeeks, weeksSize);
+                lastWeek = weeksSize - maxWeeks;
+            }
+
+            var week = [];
+            for(var i = weeksSize - 1; i >= (lastWeek + settings.weeksInSegments - 1); i -= settings.weeksInSegments) {
+                var aggregate = {additions: 0, deletion: 0, totalCommits: 0, week: 0};
+                for(var j = 0; j < settings.weeksInSegments; j++) {
+                    index = i - j;
+                    aggregate.additions += author.weeks[index].a;
+                    aggregate.deletion += author.weeks[index].d;
+                    aggregate.totalCommits += author.weeks[index].c;
+                    if (j == settings.weeksInSegments - 1) {
+                        aggregate.week = author.weeks[index].w * 1000;
+                    }
+                }
+                week.unshift(aggregate);
+                maxCommits = Math.max(maxCommits, aggregate.totalCommits);
+            }
+            author.weeks = week;
         });
-        return data;
+        return {
+            maxCommits: maxCommits,
+            data: data
+        };
     });
 }
 
--- github-commit-summary/scripts/statsViz.js
+++ github-commit-summary/scripts/statsViz.js
@@ -3,8 +3,9 @@ $(function() {
 
     settings = {
         repoName: "d3",
-        weeksInSegments: 15,
-        maxCollaborators: 10
+        weeksInSegments: 21,
+        maxCollaborators: 10,
+        maxNumberOfSegments: 15
     };
 
     var contributorChart = contributorStats(settings);
@@ -17,7 +18,7 @@ $(function() {
     $('#update-settings').on('click', function() {
         $("#vizForm :input").each(function(i, d){
             input = $(d);
-            settings[input.attr("name")] = input.val();
+            settings[input.attr("name")] = isNaN(input.val()) ? input.val() : parseInt(input.val());
         });
 
         getProcessedData(settings).then(contributorChart.update);
@@ -26,11 +27,18 @@ $(function() {
 });
 
 function contributorStats(settings) {
+    var repoName = settings.repoName;
     var width = $("#vizContainer").width(),
         height = window.innerHeight,
 
+    chartRadius = Math.min(width, height) / 2.5;
+    chartInnerRadius = chartRadius/6;
+    radiusDelta = chartRadius - chartInnerRadius;
+
     chart = d3.select("#contributor-chart");
     chart.attr("width", width).attr("height", height);
+
+    var opacity = d3.scale.linear().range([0.05,1]);
 
     var pie = d3.layout.pie()
         .sort(null)
@@ -39,65 +47,106 @@ function contributorStats(settings) {
             return 1;
         });
 
-    var arc = d3.svg.arc();
-
-    this.update = function(data) {
-        chartRadius = Math.min(width, height) / 2.5;
-        chartInnerRadius = chartRadius/6;
-        radiusDelta = chartRadius - chartInnerRadius;
-        numberOfWeeks = settings.weeksInSegments;
-        pieWidth = radiusDelta / numberOfWeeks;
-
-        arc.innerRadius(function(d, i){
-                return chartInnerRadius + (radiusDelta * i / numberOfWeeks);
+    var contributorArcs = d3.svg.arc()
+            .innerRadius(function(d, i){
+                return chartRadius + 10;
             })
             .outerRadius(function(d, i) {
-                return chartInnerRadius + (radiusDelta * i / numberOfWeeks) + pieWidth;
+                return chartRadius + 20;
+            });
+
+    var arc = d3.svg.arc();
+    var color = null;
+
+    this.update = function(processedData) {
+        if (repoName != settings.repoName || color == null) {
+            color = d3.scale.category20();
+            repoName = settings.repoName;
+        }
+
+        pieWidth = radiusDelta / settings.maxNumberOfSegments;
+
+        opacity.domain([0, processedData.maxCommits]);
+
+        arc.innerRadius(function(d, i){
+                return chartInnerRadius + (radiusDelta * i / settings.maxNumberOfSegments);
+            })
+            .outerRadius(function(d, i) {
+                return chartInnerRadius + (radiusDelta * i / settings.maxNumberOfSegments) + pieWidth;
             });
 
         var donut = chart.selectAll('.repoCommits')
                     .data(function(){
-                        pie(data).forEach(function(d){
+                        processedData.data = pie(processedData.data);
+                        processedData.data.forEach(function(d){
                             d.data.weeks.forEach(function(week){
                                 $.extend(week, d);
                             });
                         });
-                        return data;
+                        return processedData.data;
                     }, function(d){
-                        return d.author.login;
+                        return d.data.author.login;
                     });
 
         donut.enter().append("svg:g")
             .attr("class", "repoCommits")
-            .attr("transform", "translate(" + (width/2) + "," + (height/2) + ")");
+            .attr("transform", "translate(" + (width/2) + "," + (height/2) + ")")
+                .append("svg:path")
+                .attr("class", "contributor")
+                .style('fill', function(d, i){
+                    return color(d.data.author.login);
+                })
+                .attr('d', contributorArcs);
 
         donut.exit().remove();
 
-        // Start joining data with paths
         var paths = chart.selectAll('.repoCommits')
-            .selectAll('.contributors')
+            .selectAll('.commits')
             .data(function(d, i) {
-                return d.weeks;
+                return d.data.weeks;
             }, function(d,i){
                 return settings.repoName + "_" + d.data.author.login + "_" + i;
             });
 
         paths.enter()
             .append('svg:path')
-            .attr("class", "contributors");
+            .attr("class", "commits");
 
         paths
             .transition()
             .duration(500)
             .style('fill', "#000000")
             .style('fill-opacity', function(d, i){
-                return (i+1)/numberOfWeeks;
+                return opacity(d.totalCommits);
             })
             .attr('d', arc);
 
-        paths.exit()
-        .transition()
-            .duration(500).remove();
+        paths.exit().remove();
+
+        var legends = d3.select("#legends").selectAll(".legend")
+            .data(processedData.data, function(d){
+                return d.data.author.login;
+            });
+
+        legends.enter().append("div")
+            .attr("class", "legend")
+            .each(function(d, i){
+                container = d3.select(this);
+                container = container.append("a")
+                    .attr("href", d.data.author.html_url)
+                    .attr("target", "_blank");
+
+                container.append("div")
+                    .attr("class", "palette")
+                    .style("background-color", color(d.data.author.login))
+                    .text(" ");
+
+                container.append("div")
+                    .attr("class", "description")
+                    .text(d.data.author.login);
+            });
+
+        legends.exit().remove();
     }
 
     return this;
